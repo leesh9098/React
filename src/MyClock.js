e39431a0e7e5fa02d6e7e9be291b85d2033f3ebc
leesh9098 <leesh3578@gmail.com>
2021-04-14 16:19:07 +0800

    Clock by React

--- src/MyClock.js
+++ src/MyClock.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./MyClock.css";
-
+import SnowStorm from 'react-snowstorm';
+import Tilt from "react-tilt";
+import moment from "moment";
 // const React = require('react');
 // require('./MyClock.css')
 
@@ -9,7 +11,8 @@ class MyClock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: new Date()
+      time: new Date(),
+      stopList: []
     };
   }
 
@@ -29,6 +32,12 @@ class MyClock extends React.Component {
     return data < 100 ? (data < 10 ? "00" + data : "0" + data) : data;
   }
 
+  stopEventListener() {
+    this.setState({
+      stopList: this.state.stopList.concat(this.state.time)
+    })
+  }
+
   render() {
     const { time } = this.state;
     const h = time.getHours();
@@ -40,27 +49,32 @@ class MyClock extends React.Component {
     const dd = time.getDate().toString();
 
     return (
-      <div className="clockBox" style={{color:this.props.color}}>
-        <div className="clock_wrap">
-          <h1>{this.props.text}</h1>
-          <div className="date">
-            {yyyy}-{mm}-{dd}
+      <div className="clockBox" style={{ color: this.props.color }}>
+        <Tilt className="Tilt" options={{ max: 100 }} style={{ height: 250, width: 250 }} >
+          <div className="Tilt-inner">
+            <div className="clock_wrap">
+              <h1>{this.props.text}</h1>
+              <div className="date">
+                {yyyy}-{mm}-{dd}
+              </div>
+              <div className="time_wrap">
+                <span className="time">
+                  {h}:{m}:{s}
+                </span>
+                <span className="ms">{ms}</span>
+                <ul>
+                  {this.state.stopList.map((entry, idx) => {
+                    return <li key={idx}>{moment(entry).format("YYYY-MM-DD hh:mm:ss")}</li>
+                  })}
+                </ul>
+                <div className="button" onClick={this.stopEventListener.bind(this)}>Stop!</div>
+              </div>
+            </div>
           </div>
-          <div className="time_wrap">
-            <span className="time">
-              {h}:{m}:{s}
-            </span>
-            <span className="ms">{ms}</span>
-          </div>
-        </div>
+        </Tilt>
+        <SnowStorm />
       </div>
     );
-  }
-}
-
-class MyApp extends React.Component {
-  render() {
-    return <div> myapp </div>;
   }
 }
 
