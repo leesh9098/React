8902053bbf89820bae790b10f556a0455e8ebbda
leesh9098 <77374838+leesh9098@users.noreply.github.com>
2021-11-23 10:58:06 +0900

    Simple Timer

--- src/SimpleTimer.jsx
+++ src/SimpleTimer.jsx
@@ -17,23 +17,23 @@ function Timer() {
   }, []);
 
   useEffect(() => {
-      if (window.performance) {
-          if (performance.navigation.type === 1) {
-              if (window.confirm("새로고침을 하면 현재까지의 플레이 기록이 초기화 됩니다.")) {
-                  window.sessionStorage.clear();
-                  window.location.href = "/";
-              };
-          }
-      }
+    if (window.performance) {
+        if (performance.navigation.type === 1) {
+            if (window.confirm("새로고침을 하면 현재까지의 플레이 기록이 초기화 됩니다.")) {
+                window.sessionStorage.clear();
+                window.location.href = "/";
+            };
+        }
+    }
   }, []);
 
   useEffect(() => {
-      if (time.current <= 0) {
-          alert("제한 시간이 초과되었습니다. 처음 페이지로 돌아갑니다");
-          window.clearInterval(timerId.current);
-          window.sessionStorage.clear();
-          window.location.href = "/"
-      }
+    if (time.current <= 0) {
+        alert("제한 시간이 초과되었습니다. 처음 페이지로 돌아갑니다");
+        window.clearInterval(timerId.current);
+        window.sessionStorage.clear();
+        window.location.href = "/"
+    }
   }, [sec]);
   
   return (
